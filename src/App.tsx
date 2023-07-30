e8cb49f99577a1dbfd621dad40927ea6d70c3d70
alexandra <83598143+gorillagripcore@users.noreply.github.com>
2023-07-30 14:57:23 +0200

    Start of Demo Start Page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import StartPage from "./startpage";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <StartPage></StartPage>
+    </>
+  );
+}
+
+export default App;
---
+++ src/startpage.tsx
@@ -0,0 +1,34 @@
+import "./startpage.css";
+
+function StartPage() {
+  return (
+    <>
+      <img
+        className="readingnookbanner"
+        src="https://cdn.discordapp.com/attachments/1078963035630223391/1135170484397412363/readingnookbanner.png"
+      ></img>
+
+      <div className="information-grid">
+        <div className="information-item">d</div>
+        <div className="information-item">
+          <img
+            className="bookshelf1"
+            src="https://cdn-icons-png.flaticon.com/512/4344/4344790.png"
+          ></img>
+
+          <img
+            className="bookshelf2"
+            src="https://cdn.discordapp.com/attachments/1078963035630223391/1135188871651536987/4344790.png"
+          ></img>
+        </div>
+      </div>
+
+      <img
+        className="star-divider"
+        src="https://cdn.discordapp.com/attachments/1078963035630223391/1135191668145012796/asedfsed-removebg.png"
+      ></img>
+    </>
+  );
+}
+
+export default StartPage;
